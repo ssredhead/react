317acb505fc9cdbc55ed0b41f66363c7742793f0
Sophia Sommers <soommers.sophia@gmail.com>
2021-10-19 21:42:42 -0400

    show javascript

---
+++ jsInsideJSX.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// JavaScript withing JSX within JavaScript:
+// Displays 2 + 3 because of tags
+ReactDOM.render(<h1>2 + 3</h1>, document.getElementById('app'));
